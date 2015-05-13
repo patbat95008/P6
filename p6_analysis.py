d65206c9db9ce7dc336cda15a3baf66e3656be00
Patrick <pcrussel@ucsc.edu>
2015-05-13 15:04:22 -0700

    Added Base Files
    Starter code for the assignment

---
+++ p6_analysis.py
@@ -0,0 +1,12 @@
+from p6_game import Simulator
+
+ANALYSIS = {}
+
+def analyze(design):
+    sim = Simulator(design)
+    # TODO: fill in this function, populating the ANALYSIS dict
+    raise NotImplementedError
+
+def inspect((i,j), draw_line):
+    # TODO: use ANALYSIS and (i,j) draw some lines
+    raise NotImplementedError
